f2aa978d8e17a814886d89238b9131b04bbf80f1
Ilse Hernandez <serenilka@hotmail.com>
2020-04-19 22:41:27 -0600

    initial commit

---
+++ precache-manifest.3b526a9a5ca62216cb8a656ba5c89025.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032faf192e50f4817a445281c8c1927e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b19b10ea2669961abfe",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "88311830836d420738c5",
+    "url": "/static/css/main.95ab5cf1.chunk.css"
+  },
+  {
+    "revision": "8b19b10ea2669961abfe",
+    "url": "/static/js/2.24655df0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.24655df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88311830836d420738c5",
+    "url": "/static/js/main.a5625ead.chunk.js"
+  },
+  {
+    "revision": "715252fdc84a35aa1b69",
+    "url": "/static/js/runtime-main.e1306660.js"
+  }
+]);
